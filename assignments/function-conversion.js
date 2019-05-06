ac650b46c52b7ea14e1c0fdbf77de6e20c5dd9bd
Nathan Melby <nathan@Nathans-Mac-mini.local>
2019-05-06 16:01:32 -0500

    Finished

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,21 +5,38 @@
 // };
 // myFunction();
 
+const myFunction = () => console.log("function was invoked!");
+
+
+console.log (myFunction())
+
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+const anotherFunction = (param) => param;
+
+console.log (anotherFunction("example"))
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+const add =  (param1, param2) => param1 + param2;
+
+console.log(add(1,2))
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+let subtract = (param1, param2) => param1 - param2;
+
+console.log(subtract(1,2))
 
 // Stretch
 
